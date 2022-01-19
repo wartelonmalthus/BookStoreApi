469b83394b77f31ede58120a676bb8cef2e36d3b
Wartelon   Malthus <wartelon.malthus@gmail.com>
2022-01-19 16:04:28 -0300

    Adicionar arquivos de projeto.

---
+++ BookStore/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ BookStore/Models/Livro.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace BookStore.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string ISBN { get; set; }
+        public DateTime DataLancamento { get; set; }
+
+
+        //Relacionamento com CATEGORIA
+        public int CategoriaId { get; set; }
+        public Categoria categoria { get; set; }
+
+
+    }
+}
